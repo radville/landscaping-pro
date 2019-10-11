a6624a4957722478f1dd3b06dcbf0dd721940277
radville <laura.radville@gmail.com>
2019-10-11 09:12:20 -0400

    added google omniauth login

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,7 +31,8 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      # user.name = auth.info.name   # assuming the user model has a name
+      user.first_name = auth.info.name.split(" ")[0]
+      user.last_name = auth.info.name.split(" ")[1]
     end
   end
 
