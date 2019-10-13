c77a9bdbe6f6847d99daef93b213833de9f947ee
radville <laura.radville@gmail.com>
2019-10-13 07:46:47 -0400

    removed extra code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook, :google_oauth2]
@@ -13,14 +11,6 @@ class User < ApplicationRecord
 
   def full_name
     self.first_name + " " + self.last_name
-  end
-
-  def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
-      end
-    end
   end
   
   def self.from_omniauth(auth)
